54051df005e5009400e0738f019bb81c34cc170b
johnnyw <144480382+egggggggggggggggggggggg@users.noreply.github.com>
2026-07-16 00:03:44 -0400

    more ideas mainly

--- src/main.c
+++ src/main.c
@@ -1,51 +1,74 @@
-#include <stdint.h>
 #include <stdlib.h>
+
+// Layout caching
+// Retained render caching
+// Allow trivial state management
+//
 
 typedef struct t_rect {
     int x, y, width, height;
 } t_rect;
 
-typedef struct t_text {
-    char* text;
-    // reference to a font specified as the id for easier access.
-    int font_id;
-} t_text;
-
 typedef struct t_vec2 {
     int x, y;
 } t_vec2;
 
-typedef struct t_ctx {
-    void (*malloc)(size_t size);
-    void (*realloc)(void* ptr, size_t size);
-} t_ctx;
+// Stack based approach where each element has its own depth value allowing for somewhat recursive
+// tree without the recursive part?
 
-// Applies to containers so stuff like boxes and whatnot.
+// Use of a somewhat binary tree?
+// For each depth value of the same type increment a depth counter for indexing into the style array
+// for mem contiguity? Eg check the depth of the element and use that as the start. After increment
+// by 1 to allow processing of the next element. Doesn't work well as this assumes too much stuff
+// that overall makes the lib hard to use.
+// Ex :
+// window
+// ├── panel
+// │    ├── text
+// │    └── button
+// └── panel
+// would be
+// index   depth   type
+//--------------------
+// 0       0       window
+// 1       1       panel
+// 2       2       text
+// 3       2       button
+// 4       1       panel
+typedef enum {
+    CMD_RECT,
+    CMD_TEXT,
+    CMD_IMAGE,
+} t_cmd_type;
+typedef struct t_cmd_base {
+
+} t_cmd_base;
+
+typedef struct t_rect_cmd {
+
+} t_rect_cmd;
+
+typedef struct t_text_cmd {
+} t_text_cmd;
+
+typedef struct {
+    t_cmd_type type;
+    union {
+        t_rect_cmd rect;
+        t_text_cmd text;
+    };
+} t_cmd;
+
 typedef struct t_style {
-    // Void pointer to allow setting of custom font implementation.
-    void* font;
-    int (*text_width)(void* font, const char*);
-    int (*text_height)(void* font);
 
 } t_style;
+// Styles is always a multiple of 2^n - 1; Might not be ideal as this can probably lead to
+// fragmentation in some cases of overlapping elements. Might be a data structure for this type of
+// thing.
+typedef struct t_style_stack {
+    t_style *styles;
+    // Represents node depth
+    int current_depth;
+} t_style_stack;
 
-typedef struct t_element_base {
-    int    id;
-    t_rect rect;
-} t_element_base;
-
-typedef union {
-    t_element_base base;
-
-} t_element;
-
-typedef struct t_pool {
-    struct t_pool* next;
-} t_pool;
-
-t_pool* t_pool_create(void* addr, uint16_t size, uint8_t object_size) {
-    return NULL;
-}
-
-int main() {
-}
+int main() {}
